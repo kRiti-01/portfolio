2314470c0fab70cf27fd9b57b15dcd94258ea2e9
Kriti Gupta <kritigupta7031@gmail.com>
2023-05-03 19:53:52 +0530

    commits

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+
+ menuIcon.classList.toggle('bx-x');
+ navbar.classList.toggle('active');
+};
+
 // scroll section active link
 let sections=document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -20,4 +30,22 @@ window.onscroll = () => {
                 //  sticky navbar
          let header = document.querySelector('header');
          header.classList.toggle('sticky', window.scrollY>100);       
-};
+
+          
+        //  remove toggle icon and navbar when click navbar link scroll
+
+           menuIcon.classList.remove('bx-x');
+           navbar.classList.remove('active');
+           
+        };
+
+      //   scroll reveall
+      ScrollReveal({
+          reset: true,
+          distance: '80px',
+          duration: 2000,
+          delay: 100
+      });
+
+      ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+      ScrollReveal().reveal('cuboid, .project-container, .portfolio-box, .contact form' , { origin: 'bottom' });
